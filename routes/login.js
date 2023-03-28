6e1d98d4f483d7fc2dc99853ceb95ced4e06cb3d
mahdialavitabar <mehdialavitabar@gmail.com>
2023-03-28 18:41:46 +0330

    sddsds

--- routes/login.js
+++ routes/login.js
@@ -1,6 +1,25 @@
 const express = require("express");
 const passport = require("passport");
 const router = express.Router();
+const LocalStrategy = require("passport-local").Strategy;
+const User = require("../models/user");
+
+passport.use(
+  new LocalStrategy(function (username, password, done) {
+    User.findOne({ username: username }, function (err, user) {
+      if (err) {
+        return done(err);
+      }
+      if (!user) {
+        return done(null, false, { message: "Incorrect username." });
+      }
+      if (!user.validPassword(password)) {
+        return done(null, false, { message: "Incorrect password." });
+      }
+      return done(null, user);
+    });
+  })
+);
 
 /* GET login page. */
 router.get("/", function (req, res, next) {
@@ -13,7 +32,7 @@ router.post(
   passport.authenticate("bearer", { session: false }),
   function (req, res) {
     // Successful authentication, redirect to protected content
-    res.redirect("/");
+    res.redirect("/protected");
   }
 );
 
